ba395b9c4b7193789ad088a6e119ded7c6533f9b
Lena Voytek <lena.voytek@canonical.com>
2025-06-23 17:30:02 -0400

    Setup installation with install of git-ubuntu

--- src/charm.py
+++ src/charm.py
@@ -13,7 +13,7 @@ https://juju.is/docs/sdk/create-a-minimal-kubernetes-charm
 """
 
 import logging
-from typing import cast
+from os import system
 
 import ops
 
@@ -28,76 +28,22 @@ class GitUbuntuCharm(ops.CharmBase):
 
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
-        framework.observe(self.on["httpbin"].pebble_ready, self._on_httpbin_pebble_ready)
-        framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.start, self._on_start)
+        self.framework.observe(self.on.install, self._on_install)
 
-    def _on_httpbin_pebble_ready(self, event: ops.PebbleReadyEvent):
-        """Define and start a workload using the Pebble API.
-
-        Change this example to suit your needs. You'll need to specify the right entrypoint and
-        environment configuration for your specific workload.
-
-        Learn more about interacting with Pebble at at https://juju.is/docs/sdk/pebble.
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Add initial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", self._pebble_layer, combine=True)
-        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
-        container.replan()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
+    def _on_start(self, event: ops.StartEvent):
+        """Handle start event."""
         self.unit.status = ops.ActiveStatus()
 
-    def _on_config_changed(self, event: ops.ConfigChangedEvent):
-        """Handle changed configuration.
-
-        Change this example to suit your needs. If you don't need to handle config, you can remove
-        this method.
-
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        # Fetch the new config value
-        log_level = cast(str, self.model.config["log-level"]).lower()
-
-        # Do some validation of the configuration option
-        if log_level in VALID_LOG_LEVELS:
-            # The config is good, so update the configuration of the workload
-            container = self.unit.get_container("httpbin")
-            # Push an updated layer with the new config
-            try:
-                container.add_layer("httpbin", self._pebble_layer, combine=True)
-                container.replan()
-            except ops.pebble.ConnectionError:
-                # We were unable to connect to the Pebble API, so we defer this event
-                self.unit.status = ops.MaintenanceStatus("waiting for Pebble API")
-                event.defer()
-                return
-
-            logger.debug("Log level for gunicorn changed to '%s'", log_level)
-            self.unit.status = ops.ActiveStatus()
+    def _on_install(self, event: ops.InstallEvent):
+        """Handle install event."""
+        self.unit.status = ops.MaintenanceStatus("Installing git-ubuntu snap")
+        channel = self.config.get("channel")
+        if channel in ("beta", "edge", "stable"):
+            system(f"snap install git-ubuntu --{channel} --classic")
+            self.unit.status = ops.ActiveStatus("Ready")
         else:
-            # In this case, the config option is bad, so block the charm and notify the operator.
-            self.unit.status = ops.BlockedStatus(f"invalid log level: '{log_level}'")
-
-    @property
-    def _pebble_layer(self) -> ops.pebble.LayerDict:
-        """Return a dictionary representing a Pebble layer."""
-        return {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {
-                        "GUNICORN_CMD_ARGS": f"--log-level {self.model.config['log-level']}"
-                    },
-                }
-            },
-        }
+            self.unit.status = ops.BlockedStatus("Invalid channel configured.")
 
 
 if __name__ == "__main__":  # pragma: nocover
