ef7024fd061b3efdd7c51fa4ac3a5b5bcefbd6b1
Lena Voytek <lena.voytek@canonical.com>
2025-06-30 13:32:53 -0400

    Add apt install unit tests

--- tests/unit/test_package_configuration.py
+++ tests/unit/test_package_configuration.py
@@ -7,6 +7,7 @@
 
 from unittest.mock import patch
 
+from charms.operator_libs_linux.v0 import apt
 from charms.operator_libs_linux.v2 import snap
 
 import package_configuration as pkgs
@@ -28,6 +29,28 @@ def test_lp_user_config_fail(mock_system):
 
     assert not pkgs.git_update_lpuser_config("test-lp-user")
     mock_system.assert_called_once_with('git config --global gitubuntu.lpuser "test-lp-user"')
+
+
+@patch("package_configuration.apt.update")
+@patch("package_configuration.apt.add_package")
+def test_git_install_success(mock_add_package, mock_update):
+    """Test successful git install."""
+    assert pkgs.git_install()
+
+    mock_update.assert_called_once()
+    mock_add_package.assert_called_once_with("git")
+
+
+@patch("package_configuration.apt.update")
+@patch("package_configuration.apt.add_package")
+def test_git_install_fail(mock_add_package, mock_update):
+    """Test failed git install."""
+    mock_add_package.side_effect = apt.PackageError
+
+    assert not pkgs.git_install()
+
+    mock_update.assert_called_once()
+    mock_add_package.assert_called_once_with("git")
 
 
 @patch("package_configuration.snap.SnapCache")
