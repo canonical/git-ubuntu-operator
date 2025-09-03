db9d7a5fc6e6bda52b417e0b01f677876a4c45e8
Lena Voytek <lena.voytek@canonical.com>
2025-09-03 16:52:36 -0400

    Add info statements to package configuration

--- src/package_configuration.py
+++ src/package_configuration.py
@@ -24,6 +24,7 @@ def git_update_user_name_config(user: str, name: str) -> bool:
     Returns:
         True if config update succeeded, False otherwise.
     """
+    logger.info("Setting git user.name to %s for user %s.", name, user)
     return run_command_as_user(user, f"git config --global user.name '{name}'")
 
 
@@ -37,6 +38,7 @@ def git_update_user_email_config(user: str, email: str) -> bool:
     Returns:
         True if config update succeeded, False otherwise.
     """
+    logger.info("Setting git user.email to %s for user %s.", email, user)
     return run_command_as_user(user, f"git config --global user.email {email}")
 
 
@@ -50,6 +52,7 @@ def git_update_lpuser_config(user: str, lp_username: str) -> bool:
     Returns:
         True if config update succeeded, False otherwise.
     """
+    logger.info("Setting git gitubuntu.lpuser to %s for user %s.", lp_username, user)
     return run_command_as_user(user, f"git config --global gitubuntu.lpuser {lp_username}")
 
 
@@ -62,6 +65,7 @@ def git_install() -> bool:
     try:
         apt.update()
         apt.add_package("git")
+        logger.info("Installed git package.")
     except apt.PackageError as e:
         logger.error("Failed to install git from apt: %s", e)
         return False
@@ -78,6 +82,7 @@ def sqlite3_install() -> bool:
     try:
         apt.update()
         apt.add_package("sqlite3")
+        logger.info("Installed sqlite3 package.")
     except apt.PackageError as e:
         logger.error("Failed to install sqlite3 from apt: %s", e)
         return False
@@ -98,6 +103,7 @@ def git_ubuntu_snap_refresh(channel: str) -> bool:
         cache = snap.SnapCache()
         git_ubuntu_snap = cache["git-ubuntu"]
         git_ubuntu_snap.ensure(snap.SnapState.Latest, classic=True, channel=channel)
+        logger.info("Refreshed git-ubuntu snap to channel %s.", channel)
     except snap.SnapError as e:
         logger.error("Failed to refresh git-ubuntu snap to channel %s: %s", channel, e)
         return False
