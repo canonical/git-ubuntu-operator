80e6aba33d708906ff9e85cb865927fea3c4dad4
Lena Voytek <lena.voytek@canonical.com>
2025-10-01 13:47:22 -0400

    Use service list collection for stop too

--- src/git_ubuntu.py
+++ src/git_ubuntu.py
@@ -316,28 +316,19 @@ def stop_services(service_folder: str) -> bool:
     Returns:
         True if all services were stopped successfully, False otherwise.
     """
-    service_folder_path = pathops.LocalPath(service_folder)
+    service_list = _get_services_list(service_folder)
+
+    if service_list is None:
+        return False
+
     services_stopped = True
 
-    try:
-        for service_file in service_folder_path.iterdir():
-            if service_file.suffix == ".service":
-                if stop_service(service_file.name):
-                    logger.info("Stopped service %s", service_file.name)
-                else:
-                    logger.error("Failed to stop service %s", service_file.name)
-                    services_stopped = False
-            else:
-                logger.debug("Skipping non-service file %s", service_file.name)
-    except NotADirectoryError:
-        logger.error("The provided location %s is not a directory.", service_folder)
-        services_stopped = False
-    except PermissionError as e:
-        logger.error("Failed to start services due to permission issues: %s", str(e))
-        services_stopped = False
-    except FileNotFoundError:
-        logger.error("Service folder not found.")
-        services_stopped = False
+    for service in service_list:
+        if stop_service(service):
+            logger.info("Stopped service %s", service)
+        else:
+            logger.error("Failed to stop service %s", service)
+            services_stopped = False
 
     return services_stopped
 
