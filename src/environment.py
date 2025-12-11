0b2c4350ff3b5d9a5b0863521e41136db267c607
Lena Voytek <lena.voytek@canonical.com>
2025-12-11 16:40:59 -0500

    Add https proxy get function

--- src/environment.py
+++ src/environment.py
@@ -20,3 +20,18 @@ def get_juju_http_proxy_url() -> str:
         return http_proxy
 
     return ""
+
+
+def get_juju_https_proxy_url() -> str:
+    """Get the Juju-managed https proxy URL if available.
+
+    Returns:
+        The proxy URL or an empty string if it does not exist.
+    """
+    env = os.environ.copy()
+    https_proxy = env.get("JUJU_CHARM_HTTPS_PROXY")
+
+    if https_proxy:
+        return https_proxy
+
+    return ""
--- tests/unit/test_environment.py
+++ tests/unit/test_environment.py
@@ -29,3 +29,24 @@ def test_get_juju_http_proxy_url():
 
     if proxy_var_original is not None:
         os.environ["JUJU_CHARM_HTTP_PROXY"] = proxy_var_original
+
+
+def test_get_juju_https_proxy_url():
+    """Test getting Juju https proxy URL when not set."""
+    proxy_var_original = os.environ.get("JUJU_CHARM_HTTPS_PROXY")
+
+    if proxy_var_original is not None:
+        del os.environ["JUJU_CHARM_HTTPS_PROXY"]
+
+    proxy_url = env.get_juju_https_proxy_url()
+    assert proxy_url == ""
+
+    test_url = "http://httpsproxy.internal:1234"
+    os.environ["JUJU_CHARM_HTTPS_PROXY"] = test_url
+    proxy_url = env.get_juju_https_proxy_url()
+    assert proxy_url == test_url
+
+    del os.environ["JUJU_CHARM_HTTPS_PROXY"]
+
+    if proxy_var_original is not None:
+        os.environ["JUJU_CHARM_HTTPS_PROXY"] = proxy_var_original
